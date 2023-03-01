0c8bde498ed3ab7cb31aca15ef862ebbcccba79e
Katie Millar <ajh20fhu@uea.ac.uk>
2023-03-01 15:51:07 +0000

    Adding day 2 data from experiment 2 for data analysis.

--- Experiment2.R
+++ Experiment2.R
@@ -62,3 +62,52 @@ summary(exp2lm)
 #-- Using emmeans to look for significant differences 
 emmeans::emmeans(exp2lm, specs = pairwise ~ diet) 
 
+#------------ (Exp 2) Day 2 ------
+#-------- Reading the data in
+feedinge2d2 <- read_excel("data/RPFemaleFeedingE2D2.xlsx")
+#---- Making the data long
+long_feedinge2d2 <- feedinge2d2 %>% 
+  pivot_longer(cols = ("8:1S":"1:2H"), names_to = "diet", values_to = "fly_numbers")
+# summary of just day 2 
+exp2d2feeding_summary <- long_feedinge2d2 %>%  
+  group_by(diet) %>% 
+  summarise(mean = mean(fly_numbers),
+            sd = sd(fly_numbers),
+            n = n(),
+            se = sd/sqrt(n))
+
+exp2feeding_plotd2 <- exp2d2feeding_summary %>% 
+  ggplot(aes(x = diet, y = mean))+
+  geom_bar(stat = "identity",
+           fill = "skyblue",
+           colour = "#FF6863",
+           alpha = 0.6)+
+  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+                colour = "#FF6863",
+                width = 0.2)+
+  geom_jitter(data = long_feedinge2d2,
+              aes(x = diet,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)+
+  ylim(0.0, 4.0)+
+  labs(x = "Diet \n(Protein; Carbohydrate)",
+       y = "Mean (+/- S.E.) number of flies on a patch",
+       title = "")+
+  theme_minimal() 
+
+#-------------- (Exp 2) Day 2 Data analysis  -----------
+
+#------- creating a linear model for day 1 
+exp2lmd2 <- lm(fly_numbers ~ diet, data = long_feedinge2d2) 
+#------- using summary function for the model 
+summary(exp2lmd2)
+#-- Using emmeans to look for significant differences 
+emmeans::emmeans(exp2lmd2, specs = pairwise ~ diet) 
+
+
+
+exp2feeding_plot_d1 + exp2feeding_plotd2
+
