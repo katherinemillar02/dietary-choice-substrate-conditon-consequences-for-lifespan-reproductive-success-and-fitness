63d3cf810802990425c4d1a3aab1d7e67054d2fc
Katie Millar <ajh20fhu@uea.ac.uk>
2023-03-08 16:48:29 +0000

    Reading the data in for experiment 2, day 3 feeding, visualising the data and doing data analysis

--- Experiment3.R
+++ Experiment3.R
@@ -21,15 +21,15 @@ feedinge3d1 <- read_excel("data/RPFemaleFeedingE3D1.xlsx")
 #---- Making the data long
 long_feedinge3d1 <- feedinge3d1 %>% 
   pivot_longer(cols = ("1:8S":"8:1H"), names_to = "diet", values_to = "fly_numbers")
-# summary of just day 1 
-exp3feeding_summary <- long_feedinge3d1 %>%  
+# summary of just day 2 
+exp3feeding_summary_d1 <- long_feedinge3d1 %>%  
   group_by(diet) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
             n = n(),
             se = sd/sqrt(n))
 #------- Visualising the data for feeding day 1 ----------------#
-exp3feeding_plotd1 <- exp3feeding_summary %>% 
+exp3feeding_plotd1 <- exp3feeding_summary_d1 %>% 
   ggplot(aes(x = diet, y = mean))+
   geom_bar(stat = "identity",
            fill = "skyblue",
@@ -64,6 +64,60 @@ emmeans::emmeans(exp3lm, specs = pairwise ~ diet)
 #--  1:8 soft and 1:8 hard is only just significant - prefer soft to feed on 
 #-- but don't really like 8:1 soft 
 
+
+
+
+
+#------------------------------ Experiment 3 ---- 
+#----- (Exp3) Day 2 ------
+#-------- Reading the data in
+feedinge3d2 <- read_excel("data/RPFemaleFeedingE3D2.xlsx")
+#---- Making the data long
+long_feedinge3d2 <- feedinge3d2 %>% 
+  pivot_longer(cols = ("1:8S":"8:1H"), names_to = "diet", values_to = "fly_numbers")
+# summary of just day 1 
+exp3feeding_summary_d2 <- long_feedinge3d2 %>%  
+  group_by(diet) %>% 
+  summarise(mean = mean(fly_numbers),
+            sd = sd(fly_numbers),
+            n = n(),
+            se = sd/sqrt(n))
+#------- Visualising the data for feeding day 1 ----------------#
+exp3feeding_plotd2 <- exp3feeding_summary_d2 %>% 
+  ggplot(aes(x = diet, y = mean))+
+  geom_bar(stat = "identity",
+           fill = "skyblue",
+           colour = "#FF6863",
+           alpha = 0.6)+
+  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+                colour = "#FF6863",
+                width = 0.2)+
+  geom_jitter(data = long_feedinge3d2,
+              aes(x = diet,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)+
+  ylim(0.0, 4.0)+
+  labs(x = "Diet \n(Protein; Carbohydrate)",
+       y = "Mean (+/- S.E.) number of flies on a patch",
+       title = "")+
+  theme_minimal() 
+
+exp3feeding_plotd1 + exp3feeding_plotd2
+
+
+#-------------- (Exp 3) Day 1 Data analysis  -----------
+
+#------- creating a linear model for day 1 
+exp3lm_d2 <- lm(fly_numbers ~ diet, data = long_feedinge3d2)
+#------- using summary function for the model 
+summary(exp3lm)
+#-- Using emmeans to look for significant differences 
+emmeans::emmeans(exp3lm_d2, specs = pairwise ~ diet) 
+
+#--------------  combining the data 
 
 #----------- Egg count 
 
