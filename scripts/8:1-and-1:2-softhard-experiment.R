f0bbb2c593a0b45d9b97e730eebd26cd3ccedad2
Katie Millar <ajh20fhu@uea.ac.uk>
2023-03-26 17:36:44 +0100

    Fixing the code to exp2

--- scripts/8:1-and-1:2-softhard-experiment.R
+++ scripts/8:1-and-1:2-softhard-experiment.R
@@ -281,7 +281,7 @@ summary(exp2bothlmnew2)
 #emmeans::emmeans(typestogetherlm, specs = pairwise ~ food_nutrition + food_type)
 
 # summarising hard vs soft data 
-softhard_summary <- exp2both %>%  
+softhard_summary_exp3 <- exp3both %>%  
   group_by(food_type) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
@@ -289,7 +289,7 @@ softhard_summary <- exp2both %>%
             se = sd/sqrt(n))
 
 # a soft vs hard plot 
-softhard_plot <- softhard_summary %>% 
+softhard_plot_exp3 <- softhard_summary_exp3 %>% 
   ggplot(aes(x = food_type, y = mean))+
   geom_bar(stat = "identity",
            fill = "skyblue",
@@ -298,7 +298,7 @@ softhard_plot <- softhard_summary %>%
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
                 colour = "#FF6863",
                 width = 0.2)+
-  geom_jitter(data = exp2both,
+  geom_jitter(data = exp3both,
               aes(x = food_type,
                   y = fly_numbers),
               fill = "skyblue",
@@ -312,7 +312,7 @@ softhard_plot <- softhard_summary %>%
   theme_minimal() 
 
 # summarising nutrient composition data 
-nutrient_summary <- exp2both %>%  
+nutrient_summary_exp3 <- exp3both %>%  
   group_by(food_nutrition) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
@@ -320,7 +320,7 @@ nutrient_summary <- exp2both %>%
             se = sd/sqrt(n))
 
 # a nutrient plot 
-nutrient_plot <- nutrient_summary %>% 
+nutrient_plot_exp3 <- nutrient_summary_exp3 %>% 
   ggplot(aes(x = food_nutrition, y = mean))+
   geom_bar(stat = "identity",
            fill = "skyblue",
@@ -329,7 +329,7 @@ nutrient_plot <- nutrient_summary %>%
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
                 colour = "#FF6863",
                 width = 0.2)+
-  geom_jitter(data = exp2both,
+  geom_jitter(data = exp3both,
               aes(x = food_nutrition,
                   y = fly_numbers),
               fill = "skyblue",
@@ -344,7 +344,7 @@ nutrient_plot <- nutrient_summary %>%
 
 
 # using patchwork to compare soft/hardness and nutrient composition - data visualisation
-softhard_plot + nutrient_plot
+softhard_plot_exp3 + nutrient_plot_exp3
 
 
 # -------- (Exp 2) Egg counting  --------
