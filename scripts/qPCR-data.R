d013a7513274f3ee120929d5b581ab90a7baca18
Katie Millar <ajh20fhu@uea.ac.uk>
2023-04-05 15:09:04 +0100

    Analysing qPCR data

--- scripts/qPCR-data.R
+++ scripts/qPCR-data.R
@@ -3,70 +3,73 @@
 # reading the foxo data in
 foxoqPCR <- read_excel("data/qPCR_foxo_data_2.xlsx")
 # removing the na values from the foxo data and changing the file name 
-newfoxoqpcr <- na.omit(foxoqPCR)
-view(newfoxoqpcr)
+#newfoxoqpcr <- na.omit(foxoqPCR)
+#view(newfoxoqpcr)
 # making the new foxo data long 
-newlong_foxoqPCR <- newfoxoqpcr %>% 
+newlong_foxoqPCR <- foxoqPCR %>% 
   pivot_longer(cols = ("A1":"D2"), names_to = "sample", values_to = "cq")
 view(newlong_foxoqPCR)
-#  making a summary of the foxo data 
+#  making a summary of the foxo data with na values removed 
 newfoxoqPCR_summary <- newlong_foxoqPCR %>%  
   group_by(sample) %>% 
   summarise(mean = mean(cq),
             sd = sd(cq),
             n = n(),
-            se = sd/sqrt(n))
-#  visualising the foxo data 
-newfoxoqPCR_plotd1 <- newfoxoqPCR_summary %>% 
-  ggplot(aes(x = sample, y = mean))+
-  geom_bar(stat = "identity",
-           fill = "skyblue",
-           colour = "#FF6863",
-           alpha = 0.6)+
-  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
-                colour = "#FF6863",
-                width = 0.2)+
-  geom_jitter(data = long_foxoqPCR,
-              aes(x = sample,
-                  y = cq),
-              fill = "skyblue",
-              colour = "#3a3c3d",
-              width = 0.2,
-              shape = 21)+
-  ylim(0.0, 50)+
-  labs(x = "Diet Larvae were grown on",
-       y = "Mean Cq",
-       title = "")+
-  theme_minimal() 
+            se = sd/sqrt(n), na.rm = T)
+
+
+
+
 
 
 
 #  reading the fd38 data in 
 fd38qPCR <- read_excel("data/fd38_qPCR.xlsx")
-# removing the na values from the fd38 data 
-newfd38qpcr <- na.omit(fd38qPCR)
-view(newfd38qpcr)
 # making the fd38 data long
-newlong_fd38qPCR <- newfd38qpcr %>% 
+newlong_fd38qPCR <- fd38qPCR %>% 
   pivot_longer(cols = ("A1":"D2"), names_to = "sample", values_to = "cq")
 view(newlong_fd38qPCR)
 # making a summary of the fd38 data
 newfd38qPCR_summary <- newlong_fd38qPCR %>%  
   group_by(sample) %>% 
-  summarise(mean = mean(cq),
-            sd = sd(cq),
-            n = n(),
-            se = sd/sqrt(n))
+  summarise(mean = mean(cq, na.rm = T))
+# sd = sd(cq, na.rm = T),
+#           n = n(),
+#          se = sd/sqrt(n))
+#------------------------------------------
+# reading the rp20 data in
+rp20qPCR <- read_excel("data/qPCR_rp20.xlsx")
+# making the data long
+long_rp20 <- rp20qPCR %>% 
+  pivot_longer(cols = ("A1":"D2"), names_to = "sample", values_to = "cq")
+# making a summary of the data 
+rp20_summary <- long_rp20 %>%  
+  group_by(sample) %>% 
+  summarise(mean = mean(cq, na.rm = T))
+# sd = sd(cq),
+# n=n())
+#           se = sd/sqrt(n), na.rm = T)
+#------------------------------------------
+# binding the fd38 and rp20 data summaries 
+fd38_rp20 <- rbind(newfd38qPCR_summary2, rp20_summary)
+# summary of fd38 and rp20 together 
+fd38_rp20_summary <- fd38_rp20 %>%  
+  group_by(sample) %>% 
+  summarise(mean = mean(mean))
+
+
+
+
 
 
 
 #  reading the dilp3 data in
 dilp3qPCR <- read_excel("data/qPCR_dilp3.xlsx")
 # removing na values from dilp3
-newdilp3 <- na.omit(dilp3qPCR)
+# newdilp3 <- na.omit(dilp3qPCR)
 #  making the dilp3 data long
-long_dilp3 <- newdilp3 %>% 
-  pivot_longer(cols = ("A1":"D3"), names_to = "sample", values_to = "cq")
+long_dilp3 <- dilp3qPCR %>% 
+  pivot_longer(cols = ("A1":"D2"), names_to = "sample", values_to = "cq")
 view(long_dilp3)
 # making a summary of dilp3
 dilp3_summary <- long_dilp3 %>%  
@@ -74,24 +77,11 @@ dilp3_summary <- long_dilp3 %>%
   summarise(mean = mean(cq),
             sd = sd(cq),
             n = n(),
-            se = sd/sqrt(n))
+            se = sd/sqrt(n), na.rm = T)
 
 
 
-rp20qPCR <- read_excel("data/qPCR_rp20.xlsx")
 
-newrp20 <- na.omit(rp20qPCR)
-
-long_rp20 <- newrp20 %>% 
-  pivot_longer(cols = ("A1":"D3"), names_to = "sample", values_to = "cq")
-
-
-rp20_summary <- long_rp20 %>%  
-  group_by(sample) %>% 
-  summarise(mean = mean(cq),
-            sd = sd(cq),
-            n = n(),
-            se = sd/sqrt(n))
 
 
 
