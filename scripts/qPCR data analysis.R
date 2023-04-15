68cf85512dd9143e84f3284593890553de2543a2
Katie Millar <ajh20fhu@uea.ac.uk>
2023-04-15 19:01:19 +0100

    Analysing qPCR dilp3 data

--- scripts/qPCR data analysis.R
+++ scripts/qPCR data analysis.R
@@ -71,3 +71,67 @@ summary(qpcr_foxo_glm_2)
 # tukey test with emmeans 
 emmeans::emmeans(qpcr_foxo_glm_2, pairwise ~ sample) 
 emmeans::emmeans(qpcr_foxo_lm , pairwise ~ sample) 
+
+
+
+
+
+
+foxo_plot + dilp3_plot
+
+
+
+
+
+# reading the summarised dilp3 data in 
+dilp3_calcs <- read_excel("data/dilp3_calcs.xlsx")
+
+# making the  data long 
+newlong_dilp3_calcs <- dilp3_calcs %>% 
+  pivot_longer(cols = ("1:8S":"8:1H"), names_to = "sample", values_to = "cq")
+# making the data long summary
+dilp3_sum <- newlong_dilp3_calcs %>%  
+  group_by(sample) %>% 
+  summarise(mean = mean(cq, na.rm = T),
+            sd = sd(cq, na.rm = T),
+            n = n(),
+            se = sd/sqrt(n))
+# dilp3 plot 
+dilp3_plot <- dilp3_sum %>% 
+  ggplot(aes(x = sample, y = mean))+
+  geom_bar(stat = "identity",
+           fill = "skyblue",
+           colour = "#FFB6C1",
+           alpha = 0.6)+
+  theme_classic()+
+  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+                colour = "#FFB6C1",
+                width = 0.2)+
+  labs(title = "dilp3",
+       x = "Diet larvae were reared on \n(Protein: Carbohydrate/ Food Hardness)",
+       y = "Mean average relative expression 2^-ΔCt +/- S.E. fd38 + rp20")+
+  geom_jitter(data = newlong_dilp3_calcs,
+              aes(x = sample,
+                  y = cq),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
+
+
+
+
+
+
+qpcr_dilp3_lm <- lm(cq ~ sample, data = newlong_dilp3_calcs)
+qpcr_dilp3_glm <- glm(cq ~ sample, family = quasipoisson, data = newlong_dilp3_calcs)
+# performance check 
+performance::check_model(qpcr_dilp3_lm )
+performance::check_model(qpcr_dilp3_lm , check = c("qq"))
+
+summary(qpcr_dilp3_lm)
+summary(qpcr_dilp3_glm)
+
+emmeans::emmeans(qpcr_dilp3_glm , pairwise ~ sample) 
+emmeans::emmeans(qpcr_dilp3_lm , pairwise ~ sample) 
+
