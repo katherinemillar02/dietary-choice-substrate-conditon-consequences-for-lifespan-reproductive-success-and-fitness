8ef09b1b07f5f0f5ecc9a429c9ab4f92653e7409
Katie Millar <ajh20fhu@uea.ac.uk>
2023-05-04 18:02:27 +0100

    Editing the plots to see clearer differences between the two experiments

--- scripts/experiment-1b.R
+++ scripts/experiment-1b.R
@@ -115,8 +115,49 @@ exp1b_combined_foodconditions_lm_5 <- lm(formula = log(fly_numbers + 1) ~ food_t
 # ANOVA analysis using summary function
 summary(exp1b_combined_foodconditions_lm_5)
 
+# Visualizing the data 
+
+# using boxplots to look at the different variables
+
+# comparing soft and hard foods 
+foodhardness_boxplot_exp1b_feeding <- ggplot()+ 
+  geom_boxplot(exp1ball, mapping=aes(x=food_type, y=fly_numbers, fill=food_type))+
+  theme_classic()+
+  scale_fill_manual(values=c("gold", "pink"))+
+  labs(x = "Substrate condition of diet",
+       y = "Flies per food patch", 
+       title = "Food Hardness")+
+  theme(legend.position="none")+ 
+  ylim(0,9)+
+  geom_jitter(data =  exp1ball,
+              aes(x = food_type,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
 
 
+# comparing 1:2 and 1:8 diets
+foodhardness_boxplot_exp1b_oviposition <- ggplot()+ 
+  geom_boxplot(exp1ball, mapping=aes(x=food_nutrition, y=fly_numbers, fill=food_nutrition))+
+  theme_classic()+
+  scale_fill_manual(values=c("#CF9FFF", "#FF8C69"))+
+  labs(x = "Protein: Carbohydrate content of diet",
+       y = "Flies per food patch", 
+       title = "Nutrient Composition")+
+  theme(legend.position="none")+ 
+  ylim(0,9)+
+  geom_jitter(data =  exp1ball,
+              aes(x = food_nutrition,
+                  y = fly_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
+
+# using oatchwork to compare the plots together 
+foodhardness_boxplot_exp1b_feeding + foodhardness_boxplot_exp1b_oviposition
 
 
 
