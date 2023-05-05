a3c31ae35c33cf8d9a7109ecdbd2fa0d4ca3df1e
Katie Millar <ajh20fhu@uea.ac.uk>
2023-05-05 09:30:06 +0100

    Creating a new clean script for experiment 1b ovipoisition analysis

---
+++ scripts/experiment-1b-oviposition.R
@@ -0,0 +1,111 @@
+# EGG COUNTING ANALYSIS 
+# NEW SCRIPT WITH PROPERLY CHECKED MODELS 
+
+# Doing data analysis for overall egg numbers to diet
+
+# Linear model 
+eggcountinge1b_lm <- lm(egg_numbers ~ diet, data = long_egg_counting1b)
+
+# Checking the model 
+performance::check_model(eggcountinge1b_lm)
+performance::check_model(eggcountinge1b_lm, check = c("qq"))
+performance::check_model(eggcountinge1b_lm, check = c("homogeneity"))
+performance::check_model(eggcountinge1b_lm, check = c("outliers"))
+# qq and homogenity do not look great 
+
+# Trying a glm
+eggcountinge1b_glm <- glm(egg_numbers ~ diet, family = poisson, data = long_egg_counting1b)
+
+# Performance checking the glm
+performance::check_model(eggcountinge1b_glm)
+performance::check_model(eggcountinge1b_glm, check = c("qq"))
+performance::check_model(eggcountinge1b_glm, check = c("homogeneity"))
+performance::check_model(eggcountinge1b_glm, check = c("outliers"))
+# Looks a lot better 
+
+# Using summary function to check for overdispersion 
+summary(eggcountinge1b_glm)
+# Overdispersed
+
+# Model is overdispersed so using quasipoisson
+eggcountinge1b_glm_2 <- glm(egg_numbers ~ diet, family = quasipoisson, data = long_egg_counting1b)
+
+# Performance checking the glm
+performance::check_model(eggcountinge1b_glm_2)
+performance::check_model(eggcountinge1b_glm_2, check = c("qq"))
+performance::check_model(eggcountinge1b_glm_2, check = c("homogeneity"))
+performance::check_model(eggcountinge1b_glm_2, check = c("outliers"))
+# Model looks a lot better 
+
+# Getting results from summary 
+summary(eggcountinge1b_glm_2)
+
+# Tukey test to analyse everything 
+emmeans::emmeans(eggcountinge1b_glm_2, pairwise ~ diet)
+
+
+# Food condition 
+# Two factor analysis 
+
+# Separating the factors of food hardness and nutrient composition
+long_egg_counting1b$food_type <- ifelse(long_egg_counting1b$diet %in% c("1:8H", "1:2H"), "Hard", "Soft")
+long_egg_counting1b$food_nutrition <- ifelse(long_egg_counting1b$diet %in% c("1:8", "1:2H", "1:2S"), "1:2", "1:8")
+
+
+
+
+
+
+# Visualising the data fir oviposition choice 
+
+
+# Food condition analysis 
+
+# visualising the data
+
+# food hardness boxplot for oviposition - experiment 1b
+foodhardness_boxplot_exp1b_oviposition <- ggplot()+ 
+  geom_boxplot(long_egg_counting1b, mapping=aes(x=food_type, y=egg_numbers, fill=food_type))+
+  theme_classic()+
+  scale_fill_manual(values=c("lightgreen", "lightblue"))+
+  labs(x = "Substrate condition of diet",
+       y = "Eggs per food patch", 
+       title = "Food Hardness")+
+  theme(legend.position="none")+ 
+  ylim(0,200)+
+  geom_jitter(data =  long_egg_counting1b,
+              aes(x = food_type,
+                  y = egg_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
+
+
+# nutrient composition boxplot for oviposition - experiment 1b
+foodnutrition_boxplot_exp1b_oviposition  <- ggplot()+ 
+  geom_boxplot(long_egg_counting1b, mapping=aes(x=food_nutrition, y=egg_numbers, fill=food_nutrition))+
+  theme_classic()+
+  scale_fill_manual(values=c("#999933", "#44AA99"))+
+  labs(x = "Protein: Carbohydrate content of diet",
+       y = "Eggs per food patch", 
+       title = "Nutrient Composition")+
+  theme(legend.position="none")+ 
+  ylim(0,200)+
+  geom_jitter(data =  long_egg_counting1b,
+              aes(x = food_nutrition,
+                  y = egg_numbers),
+              fill = "skyblue",
+              colour = "#3a3c3d",
+              width = 0.2,
+              shape = 21)
+
+
+# Using patchwork to visualise the data together 
+foodhardness_boxplot_exp1b_oviposition + foodnutrition_boxplot_exp1b_oviposition
+
+
+
+
+
+
