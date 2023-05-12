359c976c52a4bb47bbc936a397905b04d39ef6b0
Katie Millar <ajh20fhu@uea.ac.uk>
2023-05-12 08:45:32 +0100

    Moving the old scripts to a new file

--- scripts/Testing models.R
+++
@@ -1,4 +0,0 @@
-
-
-long_feedinge1d2$food_type <- ifelse(long_feedinge1d2$diet %in% c("1:8H", "1:2H"), "hard", "soft")
-long_feedinge1d2$food_nutrition <- ifelse(long_feedinge1d2$diet %in% c("1:8", "1:2H", "1:2S"), "1:2", "1:8")
