5a30c0061ebe0ab81534c688157acbb036aab08b
Katie Millar <ajh20fhu@uea.ac.uk>
2023-05-12 15:54:02 +0100

    coding a hex sticker

--- old-scripts/hex-sticker.R
+++ old-scripts/hex-sticker.R
@@ -6,7 +6,7 @@ library(hexSticker)
 foodhardness_boxplot_exp2_feeding_s <- foodhardness_boxplot_exp2_feeding + theme_void() + theme_transparent()
 
 sticker <- sticker(foodhardness_boxplot_exp2_feeding_s, package="FoodConditionPlot", p_size=9, s_x=1.1, s_y=.75, s_width=1.4, s_height=1,
-        filename="images/ggplot2fh.png")
+        filename="images/ggplot2fhhex.png")
 
 print(sticker)
 
